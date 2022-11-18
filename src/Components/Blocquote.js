d6374dfa0e07f5ad8c21f04b1aa642377e046545
agun36 <aguntoroti10@gmail.com>
2022-11-18 21:51:38 +0100

    update build for gh-pages

--- src/Components/Blocquote.js
+++ src/Components/Blocquote.js
@@ -15,14 +15,14 @@ const Blocquote = () => {
   return (
     <div className='blockg'>
       <Row className=' p-5  justify-center align-items-center'>
-        <Col sm={12} md={6} lg={6}>
+        <Col sm={12} md={4} lg={6}>
           <h1 className='blocq--title'>Ready to give your business a boost?</h1>
           <p className='blocq--text'>
             Need further clarification or ready to start a project? Get in touch
             with us and we will be more than happy to assist.
           </p>
         </Col>
-        <Col sm={12} md={6} lg={4} className='input'>
+        <Col sm={12} md={8} lg={6} className='input'>
           <Form noValidate validated={validated} onSubmit={handleSubmit}>
             <div className='row-form mb-3 '>
               <Form.Group controlId='validationCustom01'>
